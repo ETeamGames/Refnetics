cc05b4c84c42b59fd4e2f267132bf64e43832791
ponJuice <matsu_tetsu_tty@yahoo.co.jp>
2016-07-13 22:53:51 +0900

    最初のコミット

---
+++ Assets/Scripts/TrackingPathCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackingPathCamera : MonoBehaviour {
+    public int time = 100;
+    public string PathName = "Path1";
+    public GameObject targetObj;
+
+    void Start()
+    {
+        iTween.MoveTo(this.gameObject, iTween.Hash("path", iTweenPath.GetPath(PathName), "time", time));
+    }
+
+
+    void Update()
+    {
+        if(targetObj != null)
+            this.gameObject.transform.LookAt(targetObj.transform);
+    }
+}
