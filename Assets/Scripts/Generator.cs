b5aeda43abeea17d6438029622ef1733f3844952
kanura <is0127kh@ed.ritsumei.ac.jp>
2016-07-27 17:51:08 +0900

    Generatorスクリプトを修正
    Generatorスクリプトを修正, コメントなども追加

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.IO;
 using System;
 
+/*
+ * アタッチした対象のPosition, Rotation座標を一定フレーム毎にサンプリング(ファイル出力)するクラス
+ * */
 public class Generator : MonoBehaviour {
 	FileInfo fi;
 	private string outputFileName = "outputFile.txt";
@@ -10,7 +13,9 @@ public class Generator : MonoBehaviour {
 	private Vector3 pos;
 	private Quaternion rot;
 	string inputStr;
-	private float samplingFrameNum = 100;
+	/** サンプリングレート(default FPS * minites) */
+	public int samplingFrameRate = 60*5;
+	private int samplingFrameNum;
 
 	// Use this for initialization
 	void Start () {
@@ -25,20 +30,29 @@ public class Generator : MonoBehaviour {
 		// initial writer
 		fi = new FileInfo(Application.dataPath + "/" + outputFileName);
 
-		// write initial values
-		this.WriteFile ("x,y,z");
+		// write initial label-values
+		this.WriteFile ("pos.x, pos.y, pos.z, rot.x , rot.y, rot.z");
 
 		// test part, read input-file
 		//this.ReadFile();
+
+		// other initialization
+		Init();
+	}
+
+	void Init () {
+		samplingFrameNum = samplingFrameRate;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if ( --samplingFrameNum > 0) {
+		// samplingFrameRate毎にデータを保存
+		if ( --samplingFrameNum <= 0) {
 			pos = this.transform.position;
 			rot = this.transform.rotation;
 			this.WriteVecQuaToFile (pos, rot);
-			samplingFrameNum = 100;
+			Init ();
+			Debug.Log ("data are sampled...");
 		}
 	}
 
