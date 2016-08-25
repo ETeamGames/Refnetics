5b2a7666baee7e9aba6c33b6f3525d4d42a32c3b
kanura <is0127kh@ed.ritsumei.ac.jp>
2016-08-25 10:18:45 +0900

    範囲内のBulletを反射する機能の実装
    範囲内のRefneticableObj Layerを持つBulletを反射する機能 (script) の実装
    範囲内の上記のオブジェクトをListに保持しておき、Zキーを押した際に、保存されているオブジェクト全てを反射する。
    ※現状ではクリックした位置に基づいての反射は未実装。

---
+++ Assets/Scripts/RefneticsField.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RefneticsField : MonoBehaviour {
+	public float refneticsPower = 100.0f;
+	public List<GameObject> refneticableObjList;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown (KeyCode.Z)) this.test();
+	}
+
+	void OnTriggerStay(Collider col) {
+		print(col.gameObject.name+" is staying");
+		if (!refneticableObjList.Contains (col.gameObject))
+			refneticableObjList.Add (col.gameObject);
+	}
+
+	void OnTriggerExit(Collider col) {
+		print(col.gameObject.name+" exits");
+		if (refneticableObjList.Contains (col.gameObject))
+			refneticableObjList.Remove (col.gameObject);
+	}
+
+	void test(){
+		foreach (GameObject go in refneticableObjList) {
+			Vector3 vec3 = (go.transform.position - transform.position).normalized;
+			go.GetComponent<Rigidbody>().AddForce(vec3 * refneticsPower, ForceMode.Force);
+		}
+	}
+
+}
